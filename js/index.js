a44a16e05d94a6be7c02777fbb101ae25cfd2d2e
shahroz <shahroz@gmail.com>
2024-12-16 19:23:42 +0500

    no message

--- js/index.js
+++ js/index.js
@@ -15,172 +15,63 @@ $(function () {
   });
 });
 
-$(function () {
-  var TxtRotate = function (el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = "";
-    this.tick();
-    this.isDeleting = false;
-  };
+// $(function () {
+//   var TxtRotate = function (el, toRotate, period) {
+//     this.toRotate = toRotate;
+//     this.el = el;
+//     this.loopNum = 0;
+//     this.period = parseInt(period, 10) || 2000;
+//     this.txt = "";
+//     this.tick();
+//     this.isDeleting = false;
+//   };
 
-  TxtRotate.prototype.tick = function () {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
+//   TxtRotate.prototype.tick = function () {
+//     var i = this.loopNum % this.toRotate.length;
+//     var fullTxt = this.toRotate[i];
 
-    if (this.isDeleting) {
-      this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-      this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
+//     if (this.isDeleting) {
+//       this.txt = fullTxt.substring(0, this.txt.length - 1);
+//     } else {
+//       this.txt = fullTxt.substring(0, this.txt.length + 1);
+//     }
 
-    this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+//     this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
 
-    var that = this;
-    var delta = 200 - Math.random() * 100;
+//     var that = this;
+//     var delta = 200 - Math.random() * 100;
 
-    if (this.isDeleting) {
-      delta /= 2;
-    }
+//     if (this.isDeleting) {
+//       delta /= 2;
+//     }
 
-    if (!this.isDeleting && this.txt === fullTxt) {
-      delta = this.period;
-      this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === "") {
-      this.isDeleting = false;
-      this.loopNum++;
-      delta = 100;
-    }
+//     if (!this.isDeleting && this.txt === fullTxt) {
+//       delta = this.period;
+//       this.isDeleting = true;
+//     } else if (this.isDeleting && this.txt === "") {
+//       this.isDeleting = false;
+//       this.loopNum++;
+//       delta = 100;
+//     }
 
-    setTimeout(function () {
-      that.tick();
-    }, delta);
-  };
+//     setTimeout(function () {
+//       that.tick();
+//     }, delta);
+//   };
 
-  window.onload = function () {
-    var elements = document.getElementsByClassName("txt-rotate");
-    for (var i = 0; i < elements.length; i++) {
-      var toRotate = elements[i].getAttribute("data-rotate");
-      var period = elements[i].getAttribute("data-period");
-      if (toRotate) {
-        new TxtRotate(elements[i], JSON.parse(toRotate), period);
-      }
-    }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".txt-rotate > .wrap { border-right: 0em solid #666 ; }";
-    document.body.appendChild(css);
-  };
-});
-$(function () {
-  $("#about-link").on("click", function () {
-    gsap.to("#navigation-content", 0, { display: "none", delay: 0.7 });
-    gsap.to("#navigation-content", 0, { y: "-100%", delay: 0.7 });
-    gsap.to("#hero-section", 0, { display: "none" });
-    gsap.to("#home-portfolio", 0, { display: "none" });
-    gsap.to("#blog", 0, { display: "none" });
-    gsap.to("#portfolio", 0, { display: "none" });
-    gsap.to("#breaker", 0, { display: "block" });
-    gsap.to("#breaker-two", 0, { display: "block", delay: 0.1 });
-    gsap.to("#contact", 0, { display: "none" });
-    gsap.to("#breaker", 0, { display: "none", delay: 2 });
-    gsap.to("#breaker-two", 0, { display: "none", delay: 2 });
-    gsap.to("#about", 0, { display: "block", delay: 0.7 });
-    gsap.to("#navigation-content", 0, { display: "flex", delay: 2 });
-    gsap.to("#home-footer", 0, { display: "none" });
-  });
-  $("#contact-link").on("click", function () {
-    gsap.to("#navigation-content", 0, { display: "none", delay: 0.7 });
-    gsap.to("#navigation-content", 0, { y: "-100%", delay: 0.7 });
-    gsap.to("#hero-section", 0, { display: "none" });
-    gsap.to("#home-portfolio", 0, { display: "none" });
-    gsap.to("#about", 0, { display: "none" });
-    gsap.to("#blog", 0, { display: "none" });
-    gsap.to("#portfolio", 0, { display: "none" });
-    gsap.to("#breaker", 0, { display: "block" });
-    gsap.to("#breaker-two", 0, { display: "block", delay: 0.1 });
-    gsap.to("#breaker", 0, { display: "none", delay: 2 });
-    gsap.to("#breaker-two", 0, { display: "none", delay: 2 });
-    gsap.to("#contact", 0, { display: "block", delay: 0.7 });
-    gsap.to("#navigation-content", 0, { display: "flex", delay: 2 });
-    gsap.to("#home-footer", 0, { display: "none" });
-  });
-  $("#portfolio-link").on("click", function () {
-    gsap.to("#navigation-content", 0, { display: "none", delay: 0.7 });
-    gsap.to("#navigation-content", 0, { y: "-100%", delay: 0.7 });
-    gsap.to("#hero-section", 0, { display: "none" });
-    gsap.to("#home-portfolio", 0, { display: "none" });
-    gsap.to("#about", 0, { display: "none" });
-    gsap.to("#contact", 0, { display: "none" });
-    gsap.to("#blog", 0, { display: "none" });
-    gsap.to("#breaker", 0, { display: "block" });
-    gsap.to("#breaker-two", 0, { display: "block", delay: 0.1 });
-    gsap.to("#breaker", 0, { display: "none", delay: 2 });
-    gsap.to("#breaker-two", 0, { display: "none", delay: 2 });
-    gsap.to("#portfolio", 0, { display: "block", delay: 0.7 });
-    gsap.to("#navigation-content", 0, { display: "flex", delay: 2 });
-    gsap.to("#home-footer", 0, { display: "none" });
-  });
-  $("#blog-link").on("click", function () {
-    gsap.to("#navigation-content", 0, { display: "none", delay: 0.7 });
-    gsap.to("#navigation-content", 0, { y: "-100%", delay: 0.7 });
-    gsap.to("#hero-section", 0, { display: "none" });
-    gsap.to("#home-portfolio", 0, { display: "none" });
-    gsap.to("#about", 0, { display: "none" });
-    gsap.to("#portfolio", 0, { display: "none" });
-    gsap.to("#contact", 0, { display: "none" });
-    gsap.to("#breaker", 0, { display: "block" });
-    gsap.to("#breaker-two", 0, { display: "block", delay: 0.1 });
-    gsap.to("#breaker", 0, { display: "none", delay: 2 });
-    gsap.to("#breaker-two", 0, { display: "none", delay: 2 });
-    gsap.to("#blog", 0, { display: "block", delay: 0.7 });
-    gsap.to("#navigation-content", 0, { display: "flex", delay: 2 });
-    gsap.to("#home-footer", 0, { display: "none" });
-  });
-  $("#home-link").on("click", function () {
-    gsap.to("#navigation-content", 0, { display: "none", delay: 0.7 });
-    gsap.to("#navigation-content", 0, { y: "-100%", delay: 0.7 });
-    gsap.to("#hero-section", 0, { display: "none" });
-    gsap.to("#home-portfolio", 0, { display: "none" });
-    gsap.to("#portfolio", 0, { display: "none" });
-    gsap.to("#contact", 0, { display: "none" });
-    gsap.to("#blog", 0, { display: "none" });
-    gsap.to("#breaker", 0, { display: "block" });
-    gsap.to("#breaker-two", 0, { display: "block", delay: 0.1 });
-    gsap.to("#breaker", 0, { display: "none", delay: 2 });
-    gsap.to("#breaker-two", 0, { display: "none", delay: 2 });
-    gsap.to("#header", 0, { display: "block", delay: 0.7 });
-    gsap.to("#navigation-content", 0, { display: "flex", delay: 2 });
-    gsap.to("#home-footer", 0, { display: "none" });
-  });
-});
-$(function () {
-  var body = document.querySelector("body");
-  var $cursor = $(".cursor");
-  function cursormover(e) {
-    gsap.to($cursor, {
-      x: e.clientX,
-      y: e.clientY,
-      stagger: 0.002,
-    });
-  }
-  function cursorhover(e) {
-    gsap.to($cursor, {
-      scale: 1.4,
-      opacity: 1,
-    });
-  }
-  function cursor(e) {
-    gsap.to($cursor, {
-      scale: 1,
-      opacity: 0.6,
-    });
-  }
-  $(window).on("mousemove", cursormover);
-  $(".menubar").hover(cursorhover, cursor);
-  $("a").hover(cursorhover, cursor);
-  $(".navigation-close").hover(cursorhover, cursor);
-});
+//   window.onload = function () {
+//     var elements = document.getElementsByClassName("txt-rotate");
+//     for (var i = 0; i < elements.length; i++) {
+//       var toRotate = elements[i].getAttribute("data-rotate");
+//       var period = elements[i].getAttribute("data-period");
+//       if (toRotate) {
+//         new TxtRotate(elements[i], JSON.parse(toRotate), period);
+//       }
+//     }
+//     // INJECT CSS
+//     var css = document.createElement("style");
+//     css.type = "text/css";
+//     css.innerHTML = ".txt-rotate > .wrap { border-right: 0em solid #666 ; }";
+//     document.body.appendChild(css);
+//   };
+// });
